a1cc34ee2afcfcc58a03685ff28649753d523e50
Aijing LI <renoxevans@gmaiil.com>
2026-06-16 20:56:35 +0200

    feat: add favicon

--- resources/views/wall.blade.php
+++ resources/views/wall.blade.php
@@ -3,6 +3,13 @@
 <head>
     <meta charset="utf-8">
     <title>Six Feet Under</title>
+    <!-- Favicon -->
+    <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('assets/img/favicon/apple-touch-icon.png') }}">
+    <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('assets/img/favicon/favicon-32x32.png') }}">
+    <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('assets/img/favicon/favicon-16x16.png') }}">
+    <link rel="shortcut icon" href="{{ asset('assets/img/favicon/favicon.ico') }}">
+    <link rel="manifest" href="{{ asset('assets/img/favicon/site.webmanifest') }}">
+    <meta name="theme-color" content="#ffffff">
     <style>
         * { box-sizing: border-box; }
         body {
